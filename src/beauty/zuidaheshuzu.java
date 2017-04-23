fa83946a56b62a96252b5f23f8950dc6d2c563f4
ifenghao <fenghao.zhu@gmail.com>
2017-04-23 20:06:14 +0800

    连续子数组最大和

--- src/beauty/zuidaheshuzu.java
+++ src/beauty/zuidaheshuzu.java
@@ -1,23 +1,40 @@
 package beauty;
 
+import java.util.Scanner;
+
 /**
  * Created by zfh on 17-4-22.
  */
 public class zuidaheshuzu {
-    static void solution(int[] array, int[] tail, int[] sum) {
-        tail[0] = sum[0] = array[0];
+    static int solution(int[] array) {
+        int n = array.length;
+        int[] tail = new int[n];
+        int[] total = new int[n];
+        tail[0] = total[0] = array[0];
         for (int i = 1; i < array.length; i++) {
             tail[i] = Math.max(array[i], tail[i - 1] + array[i]);
-            sum[i] = Math.max(sum[i - 1], tail[i]);
+            total[i] = Math.max(total[i - 1], tail[i]);
         }
+        return total[n - 1];
+    }
+
+    static int solution2(int[] array) {
+        int tail = array[0];
+        int total = array[0];
+        for (int i = 1; i < array.length; i++) {
+            tail = Math.max(array[i], tail + array[i]);
+            total = Math.max(total, tail);
+        }
+        return total;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{-2, 5, 3, -6, 4, -8, 6};
-        int length = array.length;
-        int[] tail = new int[length];
-        int[] sum = new int[length];
-        solution(array, tail, sum);
-        System.out.println(sum[length - 1]);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        System.out.println(solution2(array));
     }
 }
