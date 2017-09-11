d96b857ce05dda2535e1f751b6e5f45b8b1a5f27
ifenghao <fenghao.zhu@gmail.com>
2017-09-11 18:07:54 +0800

    大数开方

--- src/interview/Sqrt.java
+++ src/interview/Sqrt.java
@@ -1,5 +1,8 @@
 package interview;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * Created by zfh on 2017/8/29.
  */
@@ -28,8 +31,34 @@ public class Sqrt {
         return mid;
     }
 
+    static double rootn(double a, int r, int p) {
+        double k = 1.0, epsilon = 0.1, tmp;
+        while (p-- > 0) epsilon *= 0.1;
+        while (true) {
+            tmp = Math.pow(k, r - 1);
+            if (Math.abs(tmp * k - a) < epsilon) break;
+            k = ((r - 1) * tmp * k + a) / (r * tmp);
+        }
+        return k;
+    }
+
+    static String rootnBig(String as, int r, int p) {
+        BigDecimal a = new BigDecimal(as), tmp;
+        BigDecimal k = new BigDecimal("1.0"), e = new BigDecimal("0.1");
+        for (int i = 1; i < p; i++)
+            e = e.divide(BigDecimal.TEN, i + 1, BigDecimal.ROUND_HALF_EVEN);
+        while (true) {
+            tmp = k.pow(r - 1);
+            if (tmp.multiply(k).subtract(a).abs().compareTo(e) < 0) break;
+            k = k.add(a.subtract(tmp.multiply(k)).divide(tmp.multiply(new BigDecimal(r)), p, BigDecimal.ROUND_HALF_EVEN));
+        }
+        return k.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println(sqrt1(2, 10));
         System.out.println(sqrt2(2, 10));
+        System.out.println(rootn(2, 3, 10));
+        System.out.println(rootnBig(2 + "", 3, 10));
     }
 }
