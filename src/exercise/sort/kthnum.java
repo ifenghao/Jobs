d3bc4314bb37b21cafdec25469fde00aaf4fb63f
zhufenghao <zhufenghao@didiglobal.com>
2021-03-21 18:27:49 +0800

    选择第k大的数递归/非递归实现，选择前k小的所有数，允许/不允许修改数组

--- src/exercise/sort/kthnum.java
+++ src/exercise/sort/kthnum.java
@@ -1,5 +1,7 @@
 package exercise.sort;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -38,17 +40,69 @@ public class kthnum {
         return j;
     }
 
-    static int select(int[] array, int k, int lo, int hi) {
+    // 选择第k大的数递归实现
+    static int selectKth(int[] array, int k, int lo, int hi) {
         int j = partation2(array, lo, hi);
         if (j == k) {
             return array[j];
         } else if (j > k) {
-            return select(array, k, lo, j - 1);
+            return selectKth(array, k, lo, j - 1);
         } else {
-            return select(array, k, j + 1, hi);
+            return selectKth(array, k, j + 1, hi);
         }
     }
 
+
+    // 选择第k大的数非递归实现
+    static int selectKth2(int[] array, int k) {
+        int lo = 0, hi = array.length - 1;
+        int j = partation2(array, lo, hi);
+        while (j != k) {
+            if (j > k) {
+                hi = j - 1;
+            } else {
+                lo = j + 1;
+            }
+            j = partation2(array, lo, hi);
+        }
+        return array[j];
+    }
+
+    // 选择前k小的所有数，允许修改数组，O(n)
+    static int[] selectKall(int[] array, int k) {
+        int[] res = new int[k];
+        selectKth2(array, k); // 经过寻找第k大的数后，左边的数均比它小
+        for (int i = 0; i < k; i++) {
+            res[i] = array[i];
+        }
+        return res;
+    }
+
+    // 选择前k小的所有数，不允许修改数组，O(nlogk)
+    static int[] selectKall2(int[] array, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for (int i = 0; i < array.length; i++) {
+            if (pq.size() < k) {
+                pq.offer(array[i]);
+            } else {
+                if (pq.peek() > array[i]) {
+                    pq.poll();
+                    pq.offer(array[i]);
+                }
+            }
+        }
+        Object[] pqres = pq.toArray();
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = (int) pqres[i];
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -59,6 +113,17 @@ public class kthnum {
             array[i] = Integer.parseInt(string[i]);
         }
         int k = sc.nextInt();
-        System.out.println(select(array, n - k, 0, n - 1));
+        System.out.println(selectKth(array, n - k, 0, n - 1));
+        System.out.println(selectKth2(array, k));
+
+        int[] resk = selectKall(array, k);
+        for (int i : resk) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        resk = selectKall2(array, k);
+        for (int i : resk) {
+            System.out.print(i + " ");
+        }
     }
 }
