d0d95a8ae27f0d373af2d96d66d17eb8a7fd744e
zhufenghao <zhufenghao@didiglobal.com>
2021-04-08 16:51:13 +0800

    添加题目表述

--- src/exercise/dp/fenbinggan.java
+++ src/exercise/dp/fenbinggan.java
@@ -3,7 +3,19 @@ package exercise.dp;
 import java.util.Scanner;
 
 /**
- * Created by zfh on 17-4-12.
+ * 易老师购买了一盒饼干，盒子中一共有k块饼干，但是数字k有些数位变得模糊了，看不清楚数字具体是多少了。
+ * 易老师需要你帮忙把这k块饼干平分给n个小朋友，易老师保证这盒饼干能平分给n个小朋友。现在你需要计算出k有多少种可能的数值
+ * 输入描述:
+ * 输入包括两行：
+ *  第一行为盒子上的数值k，模糊的数位用X表示，长度小于18(可能有多个模糊的数位)
+ *  第二行为小朋友的人数n
+ * 输出描述:
+ * 输出k可能的数值种数，保证至少为1
+ * 示例1
+ * 输入
+ * 9999999999999X 3
+ * 输出
+ * 4
  */
 public class fenbinggan {
     static void solution(long[][] c, int m, int n) {
@@ -46,7 +58,7 @@ public class fenbinggan {
 //        int m = sc.nextInt();
 //        int n = sc.nextInt();
 //        long[][] c = new long[m + 1][n];
-//        solution(c, m, n);
+//        zichuan(c, m, n);
 //        System.out.println(c[m][0]);
 //        for (int i = 0; i < 1000; i++) {
 //            for (int j = 0; j < 1000; j++) {
