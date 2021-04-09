447fa33f3bd1b31c035e4947427b9d22be63b6f7
zhufenghao <zhufenghao@didiglobal.com>
2021-04-09 11:00:20 +0800

    递增栈的解释

--- src/exercise/stackqueue/maxrectangle.java
+++ src/exercise/stackqueue/maxrectangle.java
@@ -2,6 +2,30 @@ package exercise.stackqueue;
 
 import java.util.Stack;
 
+/**
+ * 最大矩形
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * 示例 1：
+ * 输入：matrix =
+ * [["1","0","1","0","0"],
+ *  ["1","0","1","1","1"],
+ *  ["1","1","1","1","1"],
+ *  ["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ * 示例 2：
+ * 输入：matrix = []
+ * 输出：0
+ * 示例 3：
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ * 示例 4：
+ * 输入：matrix = [["1"]]
+ * 输出：1
+ * 示例 5：
+ * 输入：matrix = [["0","0"]]
+ * 输出：0
+ */
 public class maxrectangle {
     static void solution(char[][] mat) {
         int m = mat.length, n = mat[0].length;
@@ -26,7 +50,7 @@ public class maxrectangle {
         int maxArea = 0, idx, tmpArea;
         for (int i = 0; i < n; i++) {
             while (!stack.isEmpty() && h[stack.peek()] > h[i]) {// 保持栈里的元素递增
-                idx = stack.pop();
+                idx = stack.pop(); // 当前元素比栈顶小，最大面积取决于当前元素，栈顶不会再使用需要弹出
                 tmpArea = stack.isEmpty() ? i * h[idx] : (i - stack.peek() - 1) * h[idx];
                 if (tmpArea > maxArea) maxArea = tmpArea;
             }
