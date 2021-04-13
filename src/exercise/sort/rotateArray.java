b0ae0ae937435828efd5e44866d2f9b8e1d18c9b
zhufenghao <zhufenghao@didiglobal.com>
2021-04-13 11:54:01 +0800

    旋转数组二分查找

--- src/exercise/sort/rotateArray.java
+++ src/exercise/sort/rotateArray.java
@@ -4,7 +4,7 @@ package exercise.sort;
  * 旋转数组求最小值：二分查找
  * 旋转数组是一个有序数组循环移位后得到
  */
-public class rotateArrayMin {
+public class rotateArray {
     static int bisearch(int[] x) {
         int lo = 0, hi = x.length - 1; // 最终lo指向前半数组末尾，hi指向后半数组开始
         if (x[lo] < x[hi]) return x[lo]; // 直接为非旋转数组，返回第一个即为最小元素
@@ -35,10 +35,34 @@ public class rotateArrayMin {
         return min;
     }
 
+    // 旋转数组中没有重复元素
+    static int bisearchTarget(int[] x, int target) {
+        int lo = 0, hi = x.length - 1; // 最终lo指向前半数组末尾，hi指向后半数组开始
+        while (lo <= hi) { // 退出时指针相等
+            int mid = lo + (hi - lo) / 2;
+            if (target == x[mid])
+                return mid;
+            if (x[mid] < x[hi]) { // mid在后半数组，无旋转情况只走此分支
+                if (target > x[mid] && target <= x[hi]) // 等号保证无旋转情况
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            } else { // mid在前半数组
+                if (target < x[mid] && target >= x[lo])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] x = {2, 3, 4, 1, 2};
         System.out.println(bisearch(x));
         int[] y = {1, 1, 1, 0, 1};
         System.out.println(bisearch(y));
+        int[] z = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(bisearchTarget(z, 0));
     }
 }
